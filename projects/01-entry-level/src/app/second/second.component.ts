5b5e060c3b7706d6e4c00ea945219ecf34342ced
enesozmus <ensozmus@gmail.com>
2024-11-11 18:41:01 +0300

    Working with Component Templates and Styles

--- projects/01-entry-level/src/app/second/second.component.ts
+++ projects/01-entry-level/src/app/second/second.component.ts
@@ -2,8 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-second',
-  templateUrl: './second.component.html',
-  styleUrl: './second.component.css'
+  // templateUrl: './second.component.html',
+  // template: '<h1>🟦 Second component!</h1>',
+  template: `
+    <h1>🟦 Second component!</h1>
+    <h2>🟦 Second component!</h2>
+  `,
+  // styleUrl: './second.component.css'
+  // styles: 'h1 { color: red; }',
+  styles: `
+    h1 { color: red; }
+    h2 { 
+      font-style: italic;
+      padding: 20px;
+      background-color: palegreen;
+      border: 1px solid green;,
+    }
+    `,
 })
 export class SecondComponent {
 
