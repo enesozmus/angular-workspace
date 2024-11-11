c50e47e16ca73cab89030aa0d3c22852704ef5ff
enesozmus <ensozmus@gmail.com>
2024-11-11 17:43:13 +0300

    Creating a New Component

---
+++ projects/01-entry-level/src/app/first/first.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrl: './first.component.css'
+})
+export class FirstComponent {
+}
+
+/* 🔴
+  class <name> {
+  }
+*/
+
+/* 🔴
+  export class <name> {
+  }
+*/
+
+/* 🔴
+  import { Component } from '@angular/core';
+
+  @Component({...})
+  export class <name> {
+  }
+*/
