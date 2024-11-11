bc12ca7dd096b020bd81f782c669535b4bfe6157
enesozmus <ensozmus@gmail.com>
2024-11-11 18:02:01 +0300

    Understanding the Role of AppModule and Component Declaration

--- projects/01-entry-level/src/app/app.module.ts
+++ projects/01-entry-level/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+// 🔴
+import { FirstComponent } from './first/first.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // 🔴
+    FirstComponent
   ],
   imports: [
     BrowserModule,
