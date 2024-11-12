56fbecead2f8c51c036ebf7216798d6c577527ba
enesozmus <ensozmus@gmail.com>
2024-11-12 12:12:13 +0300

    Signals v16

--- projects/02-entry-level/src/app/user/user.component.ts
+++ projects/02-entry-level/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,15 +11,24 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
 
-  // Using Getters For Computed Values
+  // 🟦 Using Getters For Computed Values
   get imagePath() {
     return 'users/' + this.selectedUser.avatar;
   }
 
-  // Listening to Events with Event Binding
+  // 🟦 Listening to Events with Event Binding
   onSelectUser() {
     console.log('Clicked!');
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     this.selectedUser = DUMMY_USERS[randomIndex];
+  }
+
+  // 🟦 Signals v16
+  selectedUser2 = signal(DUMMY_USERS[randomIndex]);
+  imagePath2 = computed(() => 'users/' + this.selectedUser2().avatar); // → Create a computed `Signal` which derives a reactive value from an expression.
+
+  onSelectUser2() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser2.set(DUMMY_USERS[randomIndex]);
   }
 }
