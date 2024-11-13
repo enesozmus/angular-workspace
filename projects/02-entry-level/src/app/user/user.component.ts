722fc67eb67c343ee15aba73905e13b094a46e88
enesozmus <ensozmus@gmail.com>
2024-11-13 16:01:19 +0300

    Accepting Objects As Inputs & Adding Appropriate Typings

--- projects/02-entry-level/src/app/user/user.component.ts
+++ projects/02-entry-level/src/app/user/user.component.ts
@@ -20,19 +20,22 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
 
-  // 🟦 Using Getters For Computed Values
+  //
+  // 🟦🟦 Using Getters For Computed Values
   get imagePath() {
     return 'users/' + this.selectedUser.avatar;
   }
 
-  // 🟦 Listening to Events with Event Binding
+  //
+  // 🟦🟦 Listening to Events with Event Binding
   onGetARandomUser() {
     console.log('Clicked!');
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     this.selectedUser = DUMMY_USERS[randomIndex];
   }
 
-  // 🟦 Signals v16
+  //
+  // 🟦🟦 Signals v16
   selectedUser2 = signal(DUMMY_USERS[randomIndex]);
   imagePath2 = computed(() => 'users/' + this.selectedUser2().avatar); // → Create a computed `Signal` which derives a reactive value from an expression.
 
@@ -41,7 +44,8 @@ export class UserComponent {
     this.selectedUser2.set(DUMMY_USERS[randomIndex]);
   }
 
-  // Inputs
+  //
+  // 🟦🟦 Inputs
   @Input({ required: true }) avatar!: string; // Decorator that marks a class field as an input property and supplies configuration metadata.
   @Input({ required: true }) name!: string;
 
@@ -49,8 +53,9 @@ export class UserComponent {
     return 'users/' + this.avatar;
   }
 
-  // 🟦 Signal Inputs v16
-  // avatar2 = input.required<string>();
+  //
+  // 🟦🟦 Signal Inputs v16
+  avatar2 = input.required<string>();
   // name2 = input.required<string>();
   // template → → name2(), avatar2()
   // parent template → → same approach
@@ -61,7 +66,8 @@ export class UserComponent {
   // Inputs - Get ID
   @Input({ required: true }) id!: string;
 
-  // 🟦 Outputs & Emitting Data
+  //
+  // 🟦🟦 Outputs & Emitting Data
   // @Output() select = new EventEmitter();
   @Output() select = new EventEmitter<string>();
 
@@ -76,6 +82,18 @@ export class UserComponent {
   //    this.select.emit(this.param);
   // }
 
-  // 🟦 the output() function
+  //
+  // 🟦🟦 the output() function
   select2 = output<string>(); // → You can use outputs to emit values to parent directives and component.
+
+  //
+  // 🟦🟦 Accepting Objects As Inputs & Adding Appropriate Typings
+  @Input({ required: true }) user!: {
+    id: string;
+    name: string;
+    avatar: string;
+  };
+  // this.user.id
+  // this.user.avatar
+  // [user]="users[0]"
 }
