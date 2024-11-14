1fa99267619b5b2057631debbe53c9d79bc56537
enesozmus <ensozmus@gmail.com>
2024-11-14 23:43:17 +0300

    Using localStorage for Data Storage

--- projects/02-entry-level/src/app/tasks/tasks.service.ts
+++ projects/02-entry-level/src/app/tasks/tasks.service.ts
@@ -36,8 +36,19 @@ export class TasksService {
       summary:
         'Learn all the basic and advanced features of Angular & how to apply them.',
       dueDate: '2025-12-31',
-    }
+    },
   ];
+
+  /**
+   *
+   */
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
 
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
@@ -51,9 +62,15 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
