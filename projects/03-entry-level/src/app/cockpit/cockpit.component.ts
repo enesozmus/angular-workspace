b8e40a4c269b75d3fc5a26ed6824bf99f2ab39c9
enesozmus <ensozmus@gmail.com>
2024-11-15 23:07:31 +0300

    Getting Access to the Template & DOM with ViewChild

--- projects/03-entry-level/src/app/cockpit/cockpit.component.ts
+++ projects/03-entry-level/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -35,11 +35,34 @@ export class CockpitComponent {
   // 🟦 Declaring a Template Reference Variable
   onAddServer2(test: HTMLInputElement) {
     console.log(test.value);
+    console.log('🟥');
+    console.log(this.newName2);
+    console.log(this.newName2.nativeElement);
+    console.log(this.newName2.nativeElement.value);
+
   }
+
+  // 🟦 Getting Access to the Template & DOM with @ViewChild
+  @ViewChild('newName2', { static: true }) newName2: ElementRef | any;
 }
 /*
   🟦
   🟦🟦 → Assigning an Alias to Input Properties
 
     @Input({ required: true, alias:'falanca' })
+*/
+
+/*
+  🟦
+  🟦🟦 @ViewChild
+  
+    → Bildiğimiz gibi, Angular'da düz HTML öğelerini diğer Angular bileşenleriyle birleştirerek bir bileşen şablonu tanımlarız.
+
+    → Çoğu zaman, ilgili .ts sınıfını kullanmadan sadece şablon referanslarını kullanarak birden fazla bileşeni ve HTML elementini doğrudan şablonda koordine edebiliriz. Ancak durum her zaman böyle değildir!
+
+    → Bazen ilgili elementlere veya bileşene .ts class'ından erişmemiz gerekir.
+
+  🟦
+  🟦🟦 ElementRef
+    → A wrapper around a native element inside of a View.
 */
