a56b96aa68930d2ec0616fdc818a7b16d0e85d59
enesozmus <ensozmus@gmail.com>
2024-11-16 17:22:55 +0300

    Understanding Directives

--- projects/04-entry-level/src/app/app.component.ts
+++ projects/04-entry-level/src/app/app.component.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   numbers = [1, 2, 3, 4, 5, 6, 7];
-  onlyOdd = true;
+  evenNumbers = [2, 4, 6, 8];
+  oddNumbers = [1, 3, 5, 7];
+  onlyOdd = false;
 }
