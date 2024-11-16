1b2e17c2e095a71dfb6d09740f047b1a11b44d2a
enesozmus <ensozmus@gmail.com>
2024-11-16 20:45:55 +0300

    05-directives-deep-dive

---
+++ projects/05-directives-deep-dive/src/app/auth/auth.component.ts
@@ -0,0 +1,17 @@
+import { Component, inject, signal } from '@angular/core';
+import { AuthService } from './auth.service';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css',
+})
+export class AuthComponent {
+  email = signal('');
+  password = signal('');
+  private authService = inject(AuthService);
+
+  onSubmit() {
+    this.authService.authenticate(this.email(), this.password());
+  }
+}
