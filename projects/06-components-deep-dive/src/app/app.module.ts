b00438928e6a6aa7e2de2d2394a9f629f030b2a7
enesozmus <ensozmus@gmail.com>
2024-11-19 15:47:25 +0300

    Splitting a Component into Multiple Components

--- projects/06-components-deep-dive/src/app/app.module.ts
+++ projects/06-components-deep-dive/src/app/app.module.ts
@@ -4,8 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    TrafficComponent,
+    TicketsComponent,
+    ServerStatusComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
