b312eeecd2c068d25d7edca2d1532714797e3840
enesozmus <ensozmus@gmail.com>
2024-11-19 22:07:53 +0300

    Understanding & Configuring View Encapsulation

--- projects/06-components-deep-dive/src/app/shared/control/control.component.ts
+++ projects/06-components-deep-dive/src/app/shared/control/control.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class ControlComponent {
   @Input({ required: true }) label!: string;
