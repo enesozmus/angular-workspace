1c51104a3d6511dc90c29744a245faba67161af1
enesozmus <ensozmus@gmail.com>
2024-11-21 00:00:45 +0300

    CSS Class and Style Property Bindings & Using Literal Values

--- projects/06-components-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ projects/06-components-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  // 🟡 Literal Types
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+
+  /**
+   *
+   */
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random(); // 0 - 0.99999999999999
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
