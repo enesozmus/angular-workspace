f491f3bf2f1a8a7f87279f8cdb0e96bdc4333a6c
enesozmus <ensozmus@gmail.com>
2024-11-21 17:05:51 +0300

    Extracting Input Values via Template Variables

--- projects/06-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ projects/06-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -6,14 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  // 🟡 Handling Form Submission
-  onSubmit(titleInputElement: HTMLInputElement) {
-    console.log('Hi form');
-    console.log(titleInputElement);
-    // JavaScript Object
-    console.dir(titleInputElement);
 
-    const enteredTitle = titleInputElement.value;
-    console.log(enteredTitle);
+  // 🟡 Handling Form Submission
+  // onSubmit(titleInputElement: HTMLInputElement) {
+  //   console.log('Hi form');
+  //   console.log(titleInputElement);
+  //   🟡 JavaScript Object
+  //   console.dir(titleInputElement);
+
+  //   const enteredTitle = titleInputElement.value;
+  //   console.log(enteredTitle);
+  // }
+  
+  onSubmit2(title: string, ticketText: string) {
+    console.log('🟧🟧🟧onSubmit2');
+    console.log(title);
+    console.log(ticketText);
   }
 }
