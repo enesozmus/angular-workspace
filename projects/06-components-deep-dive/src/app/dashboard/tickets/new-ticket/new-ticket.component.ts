5cfb766faa1033223e72f39defec2df23ad4982f
enesozmus <ensozmus@gmail.com>
2024-11-21 16:34:40 +0300

    Working with Template Variables

--- projects/06-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ projects/06-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -6,9 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  
-  // 🟡 Handling Form Submission 
-  onSubmit() {
+  // 🟡 Handling Form Submission
+  onSubmit(titleInputElement: HTMLInputElement) {
     console.log('Hi form');
+    console.log(titleInputElement);
+    // JavaScript Object
+    console.dir(titleInputElement);
+
+    const enteredTitle = titleInputElement.value;
+    console.log(enteredTitle);
   }
 }
