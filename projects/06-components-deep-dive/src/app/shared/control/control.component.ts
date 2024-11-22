2cbed57bf1283cffdc9fcd1c3e0b3ef6d276f4c5
enesozmus <ensozmus@gmail.com>
2024-11-22 15:46:52 +0300

    The afterRender and afterNextRender Lifecycle Functions

--- projects/06-components-deep-dive/src/app/shared/control/control.component.ts
+++ projects/06-components-deep-dive/src/app/shared/control/control.component.ts
@@ -1,5 +1,7 @@
 import {
   AfterContentInit,
+  afterNextRender,
+  afterRender,
   Component,
   contentChild,
   ContentChild,
@@ -23,6 +25,19 @@ import {
 })
 export class ControlComponent implements AfterContentInit {
   @Input({ required: true }) label!: string;
+
+  /**
+   * 🟥
+   */
+  constructor() {
+    // afterRender(() => {
+    //   console.log('afterRender');
+    // });
+
+    // afterNextRender(() => {
+    //   console.log('afterNextRender');
+    // });
+  }
 
   // label = input.required<string>();
   // label()
