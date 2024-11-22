5ea00b4736d4c527962068b21e3bd5f3192ff541
enesozmus <ensozmus@gmail.com>
2024-11-22 16:14:50 +0300

    TypeScript & Type Models

--- projects/06-components-deep-dive/src/app/dashboard/tickets/tickets.component.ts
+++ projects/06-components-deep-dive/src/app/dashboard/tickets/tickets.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Ticket } from './ticket.model';
 
 @Component({
   selector: 'app-tickets',
   templateUrl: './tickets.component.html',
-  styleUrl: './tickets.component.css'
+  styleUrl: './tickets.component.css',
 })
 export class TicketsComponent {
-
+  tickets: Ticket[] = [];
 }
