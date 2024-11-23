9b63c560822eba8d08edefea4a069d464628b596
enesozmus <ensozmus@gmail.com>
2024-11-23 16:58:04 +0300

    Updating Signal Values

--- projects/06-components-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ projects/06-components-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
   templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.css'
+  styleUrl: './ticket.component.css',
 })
 export class TicketComponent {
+  // ticket = input.required<Ticket>();
+  @Input({ required: true }) ticket!: Ticket;
 
+  detailsVisible = signal(false);
+  onToggleDetails() {
+    // this.detailsVisible.set(!this.detailsVisible());
+    this.detailsVisible.update((wasVisible) => !wasVisible);
+  }
 }
