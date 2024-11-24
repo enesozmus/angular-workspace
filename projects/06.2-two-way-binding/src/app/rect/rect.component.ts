9f36d70b40433571d201d4bb4266a8237ff4ec73
enesozmus <ensozmus@gmail.com>
2024-11-24 19:50:58 +0300

    06.2-two-way-binding

---
+++ projects/06.2-two-way-binding/src/app/rect/rect.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rect',
+  templateUrl: './rect.component.html',
+  styleUrl: './rect.component.css',
+})
+export class RectComponent {
+  // Todo: Implement custom two-way binding
+
+  onReset() {
+    // ...
+  }
+}
