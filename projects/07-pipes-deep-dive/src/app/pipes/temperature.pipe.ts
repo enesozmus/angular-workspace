a963a22a4512e321ac5c06c40e194c2b623d92af
enesozmus <ensozmus@gmail.com>
2024-11-26 18:41:34 +0300

    Accepting Parameters in Custom Pipes

--- projects/07-pipes-deep-dive/src/app/pipes/temperature.pipe.ts
+++ projects/07-pipes-deep-dive/src/app/pipes/temperature.pipe.ts
@@ -6,11 +6,32 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TemperaturePipe implements PipeTransform {
   
+  // 🟦
+  // 🟦🟦
   // transform(value: any, ...args: any[]) {
   //   return value + ' - transformed';
   // }
 
-  transform(value: string | number, ...args: any[]) {
+  // 🟦
+  // 🟦🟦
+  // transform(value: string | number, ...args: any[]) {
+  //   let val: number;
+  //   if (typeof value === 'string') {
+  //     val = parseFloat(value);
+  //   } else {
+  //     val = value;
+  //   }
+  //   const outputTemp = val * (9 / 5) + 32;
+  //   return `${outputTemp} °F`;
+  // }
+
+  // 🟦
+  // 🟦🟦
+  transform(
+    value: string | number,
+    inputType: 'cel' | 'fah',
+    outputType?: 'cel' | 'fah'
+  ) {
     let val: number;
 
     if (typeof value === 'string') {
@@ -19,8 +40,24 @@ export class TemperaturePipe implements PipeTransform {
       val = value;
     }
 
-    const outputTemp = val * (9 / 5) + 32;
+    let outputTemp: number;
 
-    return `${outputTemp} °F`;
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (5 / 9);
+    } else {
+      outputTemp = val;
+    }
+
+    let symbol: '°C' | '°F';
+
+    if (!outputType) {
+      symbol = inputType === 'cel' ? '°C' : '°F';
+    } else {
+      symbol = outputType === 'cel' ? '°C' : '°F';
+    }
+
+    return `${outputTemp} ${symbol}`;
   }
 }
