6d7ed341493b0411a52c09c96519733f563d590d
enesozmus <ensozmus@gmail.com>
2024-11-26 18:18:17 +0300

    Building a Simple Custom Pipe

--- projects/07-pipes-deep-dive/src/app/app.module.ts
+++ projects/07-pipes-deep-dive/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemperaturePipe } from './pipes/temperature.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TemperaturePipe],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
