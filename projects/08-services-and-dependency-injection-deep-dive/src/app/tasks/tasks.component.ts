0ba009055aaf0ec9980f968c20a56658ec153663
enesozmus <ensozmus@gmail.com>
2024-11-29 15:23:47 +0300

    Providing Services via the Element Injector

--- projects/08-services-and-dependency-injection-deep-dive/src/app/tasks/tasks.component.ts
+++ projects/08-services-and-dependency-injection-deep-dive/src/app/tasks/tasks.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
+// import { TasksService } from '../services/tasks.service';
 
 @Component({
    selector: 'app-tasks',
    templateUrl: './tasks.component.html',
    styleUrl: './tasks.component.css',
+   /**
+    * 🟡 Providing Services via the Element Injector
+    * → remove @Injectable({providedIn: 'root',})
+    * Sadece ilgili componenet ve onun bütün alt component'leri için ulaşılabilir.
+    * Her TasksComponent instance'ı için tamamen yalıtılmış ve ayrılmış ayrı bir service intanse'ı oluşturur. (<app-tasks />)
+    */
+   // providers: [TasksService],
 })
 export class TasksComponent {}
