1039b9f25b3b75f68b3da4ca159c18980221ddac
enesozmus <ensozmus@gmail.com>
2024-11-29 14:29:13 +0300

    Providing Services via the platformBrowserDynamic()

--- projects/08-services-and-dependency-injection-deep-dive/src/main.ts
+++ projects/08-services-and-dependency-injection-deep-dive/src/main.ts
@@ -1,9 +1,12 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module'
+import { AppModule } from './app/app.module';
+// import { TasksService } from './app/services/tasks.service';
 
+// 🟡 Angular tarafından oluşturulacak ilk kod paketine dahil edilmesine neden olur.
+// platformBrowserDynamic([{ provide: TasksService }])  // → remove @Injectable({providedIn: 'root',})
 platformBrowserDynamic()
    .bootstrapModule(AppModule, {
       ngZoneEventCoalescing: true,
    })
-   .catch(err => console.error(err))
+   .catch(err => console.error(err));
