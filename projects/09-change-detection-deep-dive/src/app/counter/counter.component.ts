39d9965a43a685448465dc6a93cb0173e2e28a57
enesozmus <ensozmus@gmail.com>
2024-11-30 17:58:06 +0300

    Telling Angular if a Certain Event Does Not Matter for Change Detection

--- projects/09-change-detection-deep-dive/src/app/counter/counter.component.ts
+++ projects/09-change-detection-deep-dive/src/app/counter/counter.component.ts
@@ -1,17 +1,32 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
 
 @Component({
    selector: 'app-counter',
    templateUrl: './counter.component.html',
    styleUrl: './counter.component.css',
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
+   // 🔴 Telling Angular if a Certain Event Does Not Matter for Change Detection
+   private zone = inject(NgZone);
    count = signal(0);
 
    // 🟡 Understanding the Importance of Writing Efficient Template Bindings
    get debugOutput() {
       console.log('🟡 [Counter] "debugOutput" binding re-evaluated.');
       return 'Counter Component Debug Output';
+   }
+
+   ngOnInit() {
+      setTimeout(() => {
+         this.count.set(0);
+      }, 4000);
+
+      // 🔴 Telling Angular if a Certain Event Does Not Matter for Change Detection
+      this.zone.runOutsideAngular(() => {
+         setTimeout(() => {
+            console.log('🔴🔴🔴🔴🔴');
+         }, 9000);
+      });
    }
 
    onDecrement() {
