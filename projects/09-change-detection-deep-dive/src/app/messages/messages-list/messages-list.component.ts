8b375970b314bb446936aa9e507283d0fae71fe9
enesozmus <ensozmus@gmail.com>
2024-12-01 12:35:08 +0300

    The Problem With OnPush, Cross-Component Data & Not Using Signals

--- projects/09-change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
+++ projects/09-change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
@@ -11,6 +11,19 @@ import { MessagesService } from '../../services/messages.service';
    templateUrl: './messages-list.component.html',
    styleUrl: './messages-list.component.css',
    standalone: false,
+   /**
+    * 🔴
+    * 🔴 Change Detection for this component runs if an input property changes and here, We have no inputs.
+    * If we trigger it manually, and that's not happening here.
+    * If we have an event in this component or a child component.
+    * And that's also not the case here.
+    * Or if a Signal changes here in this component.
+    * And that's also not the case.
+    * And that's why this component is never checked for changes and why the new message doesn't show up here.
+    * So what can you do in situations like this?
+    * 🟢 That's exactly what we'll take a look at!
+    */
+   //
    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessagesListComponent {
