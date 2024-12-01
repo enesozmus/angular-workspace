d208fd6d2cfe05066f9fc4d455058e8ac8ca2936
enesozmus <ensozmus@gmail.com>
2024-12-01 15:55:35 +0300

    The async Pipe

--- projects/09-change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
+++ projects/09-change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
@@ -34,28 +34,29 @@ import { MessagesService } from '../../services/messages.service';
     */
    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
    // messages = input.required<string[]>();
    private messagesService = inject(MessagesService);
-   private cdRef = inject(ChangeDetectorRef);
-   private destroyRef = inject(DestroyRef);
+   // private cdRef = inject(ChangeDetectorRef);
+   // private destroyRef = inject(DestroyRef);
+   messages$ = this.messagesService.messages$;
 
    // get messages() {
    //    return this.messagesService.allMessages;
    // }
 
-   messages: string[] = [];
+   // messages: string[] = [];
 
-   ngOnInit() {
-      const subscription = this.messagesService.messages$.subscribe((messages) => {
-        this.messages = messages;
-        this.cdRef.markForCheck();
-      });
-      // 🟡 clean subscription
-      this.destroyRef.onDestroy(() => {
-        subscription.unsubscribe();
-      });
-    }
+   // ngOnInit() {
+   //    const subscription = this.messagesService.messages$.subscribe((messages) => {
+   //      this.messages = messages;
+   //      this.cdRef.markForCheck();
+   //    });
+         // 🟡 clean subscription
+   //    this.destroyRef.onDestroy(() => {
+   //      subscription.unsubscribe();
+   //    });
+   //  }
 
    // 🟡 Understanding the Importance of Writing Efficient Template Binding
    get debugOutput() {
