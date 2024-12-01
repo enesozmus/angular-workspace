af2791196f338f981b41151c45f9e9427f03f4c3
enesozmus <ensozmus@gmail.com>
2024-12-01 13:02:01 +0300

    Triggering Change Detection Manually & Using RxJS Subjects

--- projects/09-change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
+++ projects/09-change-detection-deep-dive/src/app/messages/messages-list/messages-list.component.ts
@@ -1,8 +1,11 @@
 import {
    ChangeDetectionStrategy,
+   ChangeDetectorRef,
    Component,
+   DestroyRef,
    inject,
    input,
+   OnInit,
 } from '@angular/core';
 import { MessagesService } from '../../services/messages.service';
 
@@ -23,16 +26,36 @@ import { MessagesService } from '../../services/messages.service';
     * So what can you do in situations like this?
     * 🟢 That's exactly what we'll take a look at!
     */
-   //
+   /**
+    * So how can we get change detection to run again for the MessagesListComponent?
+    * Well, we in the end need to trigger it manually, and Angular gives you the tools for triggering it manually.
+    * It gives you an object you can inject into your components to trigger change detection in those components.
+    * And you can call a method on that service to trigger change detection.
+    */
    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent {
-   private messagesService = inject(MessagesService);
+export class MessagesListComponent implements OnInit {
    // messages = input.required<string[]>();
+   private messagesService = inject(MessagesService);
+   private cdRef = inject(ChangeDetectorRef);
+   private destroyRef = inject(DestroyRef);
 
-   get messages() {
-      return this.messagesService.allMessages;
-   }
+   // get messages() {
+   //    return this.messagesService.allMessages;
+   // }
+
+   messages: string[] = [];
+
+   ngOnInit() {
+      const subscription = this.messagesService.messages$.subscribe((messages) => {
+        this.messages = messages;
+        this.cdRef.markForCheck();
+      });
+      // 🟡 clean subscription
+      this.destroyRef.onDestroy(() => {
+        subscription.unsubscribe();
+      });
+    }
 
    // 🟡 Understanding the Importance of Writing Efficient Template Binding
    get debugOutput() {
--- projects/09-change-detection-deep-dive/src/app/services/messages.service.ts
+++ projects/09-change-detection-deep-dive/src/app/services/messages.service.ts
@@ -1,12 +1,17 @@
 import { Injectable, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
    providedIn: 'root',
 })
 export class MessagesService {
    // private messages = signal<string[]>([]);
-   private messages: string[] = [];
    // allMessages = this.messages.asReadonly();
+
+   // 🔴 I'll start here because it's here where the change happens.
+   // In its core, this indeed is a thing that allows you to create a wrapper around a value and it then also allows you to set up subscriptions to changes to that value.
+   messages$ = new BehaviorSubject<string[]>([]);
+   private messages: string[] = [];
    get allMessages() {
       return [...this.messages];
    }
@@ -15,5 +20,7 @@ export class MessagesService {
    addMessage(message: string) {
       // this.messages.update(prevMessages => [...prevMessages, message]);
       this.messages = [...this.messages, message];
+      // 🔴 updated messages
+      this.messages$.next([...this.messages]);
    }
 }
