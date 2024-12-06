cbe93fd8a1cc8038a50628c93d130a2f39cc2f11
enesozmus <ensozmus@gmail.com>
2024-12-06 16:25:25 +0300

    Converting Signals to Observables and Converting Observables to Signals

--- projects/10-rxjs/src/app/app.component.ts
+++ projects/10-rxjs/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { interval } from 'rxjs';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
-import { FirstRxJSComponent } from "./first-rxjs/first-rxjs.component";
-import { SecondRxJSComponent } from "./second-rxjs/second-rxjs.component";
+import { FirstRxJSComponent } from './first-rxjs/first-rxjs.component';
+import { SecondRxJSComponent } from './second-rxjs/second-rxjs.component';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,14 @@ import { SecondRxJSComponent } from "./second-rxjs/second-rxjs.component";
   imports: [FirstRxJSComponent, SecondRxJSComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
+
+  /**
+     🔴 By converting Observables to Signals in appropriate contexts, you can simplify reactivity,
+   reduce boilerplate, and make your Angular applications more efficient and expressive.
+   */
+  interval$ = interval(1000);
+  intervalSignal = toSignal(this.interval$, { initialValue: 0 });
+}
