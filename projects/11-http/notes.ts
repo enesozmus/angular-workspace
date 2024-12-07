a299d3a4e968f1b5b6e8707b32c2d7cab63d2bdd
enesozmus <ensozmus@gmail.com>
2024-12-07 20:05:55 +0300

    Understanding Communicating with Backend Services

---
+++ projects/11-http/notes.ts
@@ -0,0 +1,15 @@
+/** 🔴 Communicating
+ * 
+    Çevrimiçi dünya, çeşitli sistemlerin sahne arkasında sorunsuz bir şekilde birbirleriyle etkileşime girerek size işlevsel bir kullanıcı deneyimi sağlamasıyla
+  büyük ölçüde birbirine bağlıdır. Bunu fark etmeyebiliriz, ancak günümüzde yüzeysel olarak basit görünen birçok hafife aldığımız eylem, genellikle birden fazla
+  bileşenin uyum içinde çalışmasını gerektirir.
+  
+    Ötesinde dünya teknoloji pazarını göz önüne aldığımızda cihazlar, işletim sistemleri, programlama dilleri, uygulama geliştirme platformları, veri tabanları
+  oldukça farklılık ve çeşitlilik arz ediyor. Dolayısıyla bir yazılım ürününün platform bağımsız çalışabilmesi ve bütün bir pazara ulaşabilmesi için içindeki
+  uygulamaların bir noktada aynı dili konuşabiliyor olmaları gerekiyor. Servis verilerimizi tarayıcılara ve tüm bu modern cihaz uygulamalarına hızlı, güvenli
+  ve basit bir şekilde sunmak istiyorsak, tüm tarayıcılarla ve tüm bu modern cihazlarla uyumlu protokellere ve bir hizmet, arayüz sağlayan API'lara sahip olmalıyız.
+  Bunlar olmadan, modern internette keyfini çıkardığımız zengin etkileşimli deneyimlere sahip olamazdık.
+  
+    Dolayısıyla API'lar bu ortak dile, iletişim kurabilme yeteneğine her platform tarafından desteklenecek notasyonlar şeklinde protokoller ile hizmet eden servislerdir,
+  iletişim araçlarıdır.
+ */
