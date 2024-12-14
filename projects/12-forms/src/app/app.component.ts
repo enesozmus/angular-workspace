0984bdca08e7405fad3dcde970402b3f3f0233c6
enesozmus <ensozmus@gmail.com>
2024-12-14 15:12:41 +0300

    Connecting & Registering Inputs for a Complex Form

--- projects/12-forms/src/app/app.component.ts
+++ projects/12-forms/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ActorFormComponent } from './actor-form/actor-form.component';
 import { LoginComponent } from "./auth/login/login.component";
 import { ReactiveLoginComponent } from "./auth/reactive-login/reactive-login.component";
+import { RegisterComponent } from "./auth/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ActorFormComponent, LoginComponent, ReactiveLoginComponent],
+  imports: [ActorFormComponent, LoginComponent, ReactiveLoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
