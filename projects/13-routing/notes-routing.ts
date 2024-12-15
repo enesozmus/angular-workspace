5c891952a4258493bf61c200a00a161a91994e27
enesozmus <ensozmus@gmail.com>
2024-12-15 12:36:35 +0300

    Angular Routing

---
+++ projects/13-routing/notes-routing.ts
@@ -0,0 +1,9 @@
+/** 🔴 Angular Routing
+ * Routing, kullanıcının single-page bir uygulamada gördüklerini değiştirmenize yardımcı olur.
+ * Single-page bir uygulamada, yeni bir sayfa almak için sunucuya gitmek yerine, belirli component'lere
+ karşılık gelen ekran bölümlerini göstererek veya gizleyerek kullanıcının gördüklerini değiştirirsiniz.
+ * Angular URL'i izleyerek ve manipüle ederek farklı URL'ler için farklı component'leri render edebilir.
+ * 
+ * Bir view'dan diğerine gezinmeyi (the navigation) yönetmek için Angular Router'ı kullanırsınız.
+ * Angular Router, bir tarayıcı URL'ini görünümü değiştirme talimatı olarak yorumlayarak gezinmeyi etkinleştirir.
+ */
