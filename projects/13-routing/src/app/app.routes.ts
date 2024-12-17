6eab4cb96c14e443cd820b4925d3878ce02093c6
enesozmus <ensozmus@gmail.com>
2024-12-17 11:16:00 +0300

    Splitting Route Definitions Across Multiple Files

--- projects/13-routing/src/app/app.routes.ts
+++ projects/13-routing/src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { routes as userRoutes } from './users/users.routes';
+// import { TasksComponent } from './tasks/tasks.component';
+// import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 // export const routes: Routes = [];
@@ -11,21 +12,22 @@ export const routes: Routes = [
   {
     path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
-    children: [
-      {
-        path: '',
-        // redirectTo: '/users/u1', pathMatch: 'full'
-        component: NoTaskComponent,
-      },
-      {
-        path: 'tasks', // <your-domain>/users/<uid>/tasks
-        component: TasksComponent,
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent,
-      },
-    ],
+    children: userRoutes,
+    // children: [
+    //   {
+    //     path: '',
+    //     // redirectTo: '/users/u1', pathMatch: 'full'
+    //     component: NoTaskComponent,
+    //   },
+    //   {
+    //     path: 'tasks', // <your-domain>/users/<uid>/tasks
+    //     component: TasksComponent,
+    //   },
+    //   {
+    //     path: 'tasks/new',
+    //     component: NewTaskComponent,
+    //   },
+    // ],
   },
   {
     path: '', // <your-domain>/
