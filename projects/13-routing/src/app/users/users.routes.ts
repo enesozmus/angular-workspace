269210ed032a4186722860022fd5ba383b563da7
enesozmus <ensozmus@gmail.com>
2024-12-18 15:59:15 +0300

    An Example of CanDeactivate Guard

--- projects/13-routing/src/app/users/users.routes.ts
+++ projects/13-routing/src/app/users/users.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 
 import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
-import { NewTaskComponent } from '../tasks/new-task/new-task.component';
+import {
+  canLeaveEditPage,
+  NewTaskComponent,
+} from '../tasks/new-task/new-task.component';
 
 export const routes: Routes = [
   {
@@ -22,5 +25,6 @@ export const routes: Routes = [
   {
     path: 'tasks/new',
     component: NewTaskComponent,
+    canDeactivate: [canLeaveEditPage],
   },
 ];
