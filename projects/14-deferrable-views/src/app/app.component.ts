55ccd15533e4012caf5ed1bbf2f00f4fff5fde0e
enesozmus <ensozmus@gmail.com>
2024-12-19 12:28:40 +0300

    14-deferrable-views

---
+++ projects/14-deferrable-views/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [WelcomeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {}
